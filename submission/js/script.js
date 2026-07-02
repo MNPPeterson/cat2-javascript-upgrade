c77339c37b28871d62b8ae9f54875393f2b07c4b
MNPPeterson <petersonngwani@gmail.com>
2026-07-02 19:19:17 +0300

    Toggling the banner

---
+++ submission/js/script.js
@@ -0,0 +1,10 @@
+const bannerImg = document.getElementById('bannerimg');
+const bannerText = document.getElementById('bannertext');
+
+
+bannerImg.addEventListener('click', function() {
+  
+  
+  bannerText.classList.toggle('show');
+  
+});
