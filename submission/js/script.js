74c31c2ef904e75f467cb4cbf1dd1072e2350e52
MNPPeterson <petersonngwani@gmail.com>
2026-07-02 22:26:32 +0300

    Adding and removing elements

--- submission/js/script.js
+++ submission/js/script.js
@@ -8,3 +8,44 @@ bannerImg.addEventListener('click', function() {
   bannerText.classList.toggle('show');
   
 });
+
+let wishlistinput = document.getElementById('WishlistInput');
+let addbutton = document.getElementById('AddButton');
+let WishContainer = document.getElementById('wishlistcontainer');
+
+addbutton.addEventListener("click",function(event){
+
+    let wishlistInputValue = wishlistinput.value;
+    if(wishlistInputValue !="" ){
+        let li = document.createElement("li");
+        let button = document.createElement("button");
+
+        li.textContent = wishlistInputValue;
+        button.textContent ="Delete";
+
+         button.addEventListener("click",event =>{
+            li.remove()
+        })
+
+
+
+
+
+
+        li.appendChild(button);
+        WishContainer.appendChild(li);
+
+
+
+
+        wishListInput.value ="";
+    }
+    
+
+
+    
+
+
+
+
+})
