a6940ab53580d23a828adb5a2890c0d534d38fc5
MNPPeterson <petersonngwani@gmail.com>
2026-07-03 11:56:01 +0300

    Persistent state

--- submission/js/script.js
+++ submission/js/script.js
@@ -2,28 +2,28 @@ const bannerImg = document.getElementById('bannerimg');
 const bannerText = document.getElementById('bannertext');
 
 
-bannerImg.addEventListener('click', function() {
-  
-  
-  bannerText.classList.toggle('show');
-  
+bannerImg.addEventListener('click', function () {
+
+
+    bannerText.classList.toggle('show');
+
 });
 
 let wishlistinput = document.getElementById('WishlistInput');
 let addbutton = document.getElementById('AddButton');
 let WishContainer = document.getElementById('wishlistcontainer');
 
-addbutton.addEventListener("click",function(event){
+addbutton.addEventListener("click", function (event) {
 
     let wishlistInputValue = wishlistinput.value;
-    if(wishlistInputValue !="" ){
+    if (wishlistInputValue != "") {
         let li = document.createElement("li");
         let button = document.createElement("button");
 
         li.textContent = wishlistInputValue;
-        button.textContent ="Delete";
+        button.textContent = "Delete";
 
-         button.addEventListener("click",event =>{
+        button.addEventListener("click", event => {
             li.remove()
         })
 
@@ -38,31 +38,52 @@ addbutton.addEventListener("click",function(event){
 
 
 
-        wishListInput.value ="";
+        wishListInput.value = "";
     }
-    
+
 })
 
 let submitbtn = document.querySelector("#InfoForm button");
 let InfOutput = document.querySelector("#InfoOutput");
 let InfForm = document.querySelector("#InfoForm");
 
-InfForm.addEventListener("submit",event =>{
+const name = document.querySelector("#CookName");
+const email = document.querySelector("#CookEmail");
+
+const savedName = localStorage.getItem('savedCookName');
+const savedEmail = localStorage.getItem('savedCookEmail');
+
+if (savedName) name.value = savedName;
+if (savedEmail) email.value = savedEmail;
+
+name.addEventListener('input', () => {
+    localStorage.setItem('savedCookName', nameField.value);
+});
+
+email.addEventListener('input', () => {
+    localStorage.setItem('savedCookEmail', emailField.value);
+});
+
+
+InfForm.addEventListener("submit", event => {
     event.preventDefault();
 
-    const name = document.querySelector("#CookName");
-    const email = document.querySelector("#CookEmail");
-    if(name ==""|| email ==""){
-        InfOutput.textContent="Fill out both attributes pls"
 
-    }else{
+    if (name.value == "" || email.value == "") {
+        InfOutput.textContent = "Fill out both attributes pls"
 
-    InfOutput.innerHTML=
-    "<p>Name:"+name.value+"</p>"+
-    "<p>Email:"+email.value+"</p>"
+    } else {
 
-    name.value="";
-    email.value ="";
+        InfOutput.innerHTML =
+            "<p>Name:" + name.value + "</p>" +
+            "<p>Email:" + email.value + "</p>"
+
+
+        localStorage.removeItem('savedCookName');
+        localStorage.removeItem('savedCookEmail');
+
+        name.value = "";
+        email.value = "";
 
 
 
